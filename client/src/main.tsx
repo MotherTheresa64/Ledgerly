25dda28615aaca1ca443e77f02d8ba18e814cb68
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2026-08-19 04:37:07 -0500

    feat: mount persistent theme switcher

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ThemeSwitcher from './ThemeSwitcher'
 import './styles.css'
 import './consumer.css'
+import './themes.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ThemeSwitcher />
   </React.StrictMode>,
 )
