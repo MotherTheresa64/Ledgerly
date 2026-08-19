910f0d17c8689a3cbd8e4796201f627781103ed7
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2026-08-19 06:35:25 -0500

    feat: paginate transaction history
    * feat: paginate transaction history
    
    * style: add responsive transaction pagination
    
    * feat: enable transaction pagination

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import ThemeSwitcher from './ThemeSwitcher'
 import { installTransactionTextLimits } from './inputGuardrails'
+import { installTransactionPagination } from './transactionPagination'
 import './styles.css'
 import './consumer.css'
 import './themes.css'
 import './guardrails.css'
+import './pagination.css'
 
 installTransactionTextLimits()
+installTransactionPagination()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
