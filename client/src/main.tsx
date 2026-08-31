de48c4de2e6d1df33d7f8477592ddfc0f65dfd92
Noah Ragan <64329247+MotherTheresa64@users.noreply.github.com>
2026-08-31 05:22:05 -0500

    Enable hamburger navigation on mobile

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,14 +4,17 @@ import App from './App'
 import ThemeSwitcher from './ThemeSwitcher'
 import { installTransactionTextLimits } from './inputGuardrails'
 import { installTransactionPagination } from './transactionPagination'
+import { installMobileNavigation } from './mobileNavigation'
 import './styles.css'
 import './consumer.css'
 import './themes.css'
 import './guardrails.css'
 import './pagination.css'
+import './mobileNavigation.css'
 
 installTransactionTextLimits()
 installTransactionPagination()
+installMobileNavigation()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
